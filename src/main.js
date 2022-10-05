8471a4ac5b73867700ed62a324a8b5ac93b5739c
Jorge G <jorgegomezweb@gmail.com>
2022-10-05 08:42:25 +0200

    feat: add Vue toast package

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue';
 import router from './router';
 import store from './store/index.js';
 import './index.css';
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 
 import BaseButton from './components/ui/BaseButton.vue';
 
@@ -12,5 +14,6 @@ app.component('base-button', BaseButton);
 
 app.use(store);
 app.use(router);
+app.use(Toast);
 
 app.mount('#app');
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,12 +1,17 @@
 import { v4 as uuidv4 } from 'uuid';
+import { useToast } from 'vue-toastification';
+
+const toast = useToast();
 
 export default {
   addTask(context, data) {
     const taskData = { ...data, id: uuidv4() };
 
     context.commit('addTask', taskData);
+    toast.success('Tarea añadida!');
   },
   setUserData(context, data) {
     context.commit('setUserData', data);
+    toast.success('Datos de usuario introducidos!');
   },
 };
